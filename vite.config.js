af1f2ee7563325af1cae40bb8c22f5031cfd0cdb
Juanwei Hu <Juanweih@uci.edu>
2026-06-05 19:27:29 -0700

    fix url v2

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react(),
-    tailwindcss()
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/ymoller/' : '/',
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
-})
+}))
